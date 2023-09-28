c3147e3b47e220bfc6c18bd815384f433bc22bcd
fcdeclas <145944254+fcdeclas@users.noreply.github.com>
2023-09-29 07:54:49 +0800

    Add files via upload

---
+++ Fredmar.py
@@ -0,0 +1,74 @@
+lbs = 350
+kg =  lbs/2.205
+kg = round(kg,2)
+lbs = str(lbs)
+kg = str(kg)
+print("Weight in Pounds (lbs) = " + lbs)
+print("Weight converted to Kilogram (kg) = " + kg)
+
+print("==================================")
+
+mi = 21
+km = mi*1.609
+km = round(km,2)
+mi = str(mi)
+km = str(km)
+print("Length in Miles (mi) " + mi)
+print("Length in Kilometer (km) " + km)
+
+print("==================================")
+
+Fahrenheit = 10000
+Celsius = (5/9)*(Fahrenheit - 32)
+Fahrenheit = str(Fahrenheit)
+Celsius = round(Celsius)
+Celsius = str(Celsius)
+print("Temperature to Fahrenheit (°F)" + Fahrenheit)
+print("Temperature to Celsius (°C)" + Celsius)
+
+print("==================================")
+
+Student1 = "25"
+Student2 = "27"
+Student3 = "24"
+Student4 = "25"
+Student5 = "23"
+Student6 = "28"
+Student7 = "26"
+Student8 = "28"
+Student9 = "24"
+Student10 = "23"
+average = "23.23"
+
+print("Age of Student 1: " + Student1)
+print("Age of Student 2: " + Student2)
+print("Age of Student 3: " + Student3)
+print("Age of Student 4: " + Student4)
+print("Age of Student 5: " + Student5)
+print("Age of Student 6: " + Student6)
+print("Age of Student 7: " + Student7)
+print("Age of Student 8: " + Student8)
+print("Age of Student 9: " + Student9)
+print("Age of Student 10: " + Student10)
+print("The average age of the students is: " + average)
+
+print("==================================")
+
+character1 = "<Ras>"
+character2 = "<Vildred>"
+character3 = "<Diene>"
+character4 = "<Mercedes>"
+enemy1 = "<Archdemon>"
+weapon1 = "<Archdemon's might>"
+weapon2 = "<Sanctum/s>"
+weapon3 = "<Dark stone>"
+weapon4 = "<guardian>"
+weapon5 = "<>"
+
+print(" " + character1 + " and the heirs with him, Ruele and Kise are preparing for the fight with the "+ enemy1 + ". Their comrade who has been travelling with them " + character2 + " thus far decides to stay back to clear the monsters and urges the heirs to defeat the " + enemy1 + ". Unbeknownst to the heirs, Acolyte Kayron appears before " + character2 + " before killing him whilst he was fighting. The heirs travel ahead and begin fighting the " + enemy1 + ". However, during the fight, Ruele is killed after her barrier is shattered by the " + enemy1 + ". The fight suddenly flips around after the " + enemy1 + " killed Ruele and all the heirs are killed in the fight. Diche resets the 6th world, and creates the current scenario, the 7th world.")
+print("Two decades later, " + character1 + " wakes up in the World's End Pub, surrounded by the bar owners. He is told that he had slept for the past twenty years and was moved to the pub due to the lack of funding to keep him in the Tirel Castle. He is greeted by " + character2 + ", who is suspiciously still alive and noted by " + character1 + " that he hasn't changed his appearance since the battle with the " + enemy1 + ". " + character2 + " escorts " + character1 + " to Tirel Castle, meeting the young Prince Aither and one the heirs Iseria. Together, they find his " + weapon4 + " Arkasus, who has been living off snacks for the past two decades. Arriving at Tirel Castle, " + character1 +" is granted an audience with Queen " + character3 + " , the person who ended the Archdemon War.")
+print(" " + character3 + "  explains that she merely separated the " + enemy1 + " into parts, with them all being held in " + weapon2 + " in different areas of Ritania. She offers to take " + character1 + " to the Ezeran Sanctum the next day to destroy the " + weapon1 + " held there. Before turning in for the night, " + character1 + " meets a wraith hanging around the gardens in Tirel Castle. She tells " + character1 + " that her name is " + character4 + " and is being kept at Tirel Castle under the hospitality of the Queen.")
+print("The next day, the party sets out for the " + weapon2 + " accompanied by " + character2 + " and " + character4 + ". However, tragedy strikes when " + character2 + " kills " + character3 + " after " + character1 + " tells " + character2 + " his resolve to keep opposing the threat of the " + enemy1 + ". " + character2 + " absorbs the " + weapon1 + " and escapes as a traitor to Ezera.")
+print("Despite the death of Queen " + character3 + ", " + character1 + " heads to Reingar with " + character4 + " and Arky. They save a merchant named Gusma and he leads them to the entrance of Reingar. However, upon arriving at Reingar, they find out that their identification documents have been taken and are branded as illegal aliens and end up in an interrogation chamber. There, down in the chambers, they meet Yuna Woo, another heir and the next President. They find out that she has been resisting efforts to become the President and is in the chambers due to her creations running berserk. She asks them to help her break out because she is worried that the Reingar Security Club might break her machine. They help her escape the interrogation chambers and tracks down her malfunctioning creation. They discover that the interference was from a " + weapon3 + ", leading them to return back to Reingar to announce their discoveries, only to discover that the merchant Gusma and a member of the Security Club were planning to summon an Acolyte to become the President of Reingar. " + character1 + " is unable to prevent the summoning and Acolyte Tenebria is summoned. She proceeds to destroy half the city in exchange for handing over the position of President over to Plum, the member of the security club who summoned her. Yuna, who finally acknowledges her position as President, summons " + weapon4 + "Kazran. Kazran reverses time to undo the damage and Tenebria, who is unable to keep her current form, leaves. Yuna becomes the President of Reingar and " + character1 + " destroys the Archdemon's Might.")
+print("The team head down to Savara to investigate the trading situation of the " + weapon3 + ". Along the way, they run into Cartuja, a mercenary heading back to Ki Rubaran, the main town in Savara. They are picked up by the representatives of the Afih merchant guild after they are attacked by a mercenary and meets Purrgis, a Suin claiming to be a heir and that his " + weapon4 + " was missing. They investigate the sewers of the town and find " + character2 + ", who has slain the " + weapon4 + " Hainen and run into a homunculus who resembles Kise but is named Sigret. They fight her but fail to catch up to " + character2 + ". Purrgis proceeds to betray the group and attacks them to make time for Master Nehenqui, the mastermind behind the Dark Stone trade, to get away. When they finally catch up to him, they find that they have a hard time pinning any of the crimes done by Nehenqui. The Afih merchant guild requests " + character1 + " to investigate the origins of the " + weapon3 + ".")
+print("Teaming up with Cartuja, the group heads into Solayu Swamp to investigate the origins of the " + weapon3 + ". They discover that Suin have been kidnapped erratically. " + character4 + " at this point is struck unconscious from the large amounts of dark energy flowing out of the forest and is visited by visions of people mistrusting her and considers joining the " + enemy1 + ". However a vision of " + character1 + " appears telling her that he trusts her and she wakes up. Ras and Cartuja discover that the cause of the Dark Stones were from the Ilyros Church, where they've sacrificed Suin to create Dark Stones. They free the remaining Suin and imprison the Head Priest, but not before Acolyte " + weapon5 + " was summoned.")
